00786ca20e0012497a3cf0d2472df482b4f3c366
CHOIisaac <ehdrhkdls@gmail.com>
2022-10-29 16:15:56 +0900

    part3 CRUD end

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
@@ -29,6 +30,30 @@ public class BoardController {
         log.info("register: "+boardVO);
         service.register(boardVO);
         rttr.addFlashAttribute("result", boardVO.getBno());
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/get")
+    public void get(@RequestParam("bno") Long bno, Model model){
+        log.info("/get");
+        model.addAttribute("board",service.get(bno));
+    }
+
+    @PostMapping("/modify")
+    public String modify(BoardVO boardVO, RedirectAttributes rttr){
+        log.info("modify: "+boardVO);
+        if(service.modify(boardVO)){
+            rttr.addFlashAttribute("result", "success");
+        }
+        return "redirect:/board/list";
+    }
+
+    @PostMapping("/remove")
+    public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr){
+        log.info("remove.........");
+        if (service.remove(bno)){
+            rttr.addFlashAttribute("result", "success");
+        }
         return "redirect:/board/list";
     }
 }
